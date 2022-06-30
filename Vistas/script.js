259660dfd5cddd1695f5414fda38f172f9711d33
Angel Garcia <angeliux.jossue@gmail.com>
2022-06-29 20:55:20 -0500

    primera version

---
+++ Vistas/script.js
@@ -0,0 +1,10 @@
+// Functions Open Menu
+document.querySelector('.mobile__burguer').addEventListener('click', () => {
+    document.querySelector('#nav__mobile').classList.toggle('show');
+});
+
+// Functions close Menu
+document.querySelector('.mobile__close').addEventListener('click', () => {
+    document.querySelector('#nav__mobile').classList.toggle('show');
+})
+
